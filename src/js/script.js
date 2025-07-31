8005e9d2130f318537c93dd52a38d5a959bbc0a1
mosfi99 <123741608+mosfi99@users.noreply.github.com>
2025-07-31 11:32:06 +0200

    Refactor Game initialization and player handling; update showGameStatus to use player names from input fields.

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,5 @@
 const BOARD_SIZE = 3;
+let Game;
 
 // Gameboard as an IIFE Module to handle board state
 const Gameboard = (function () {
@@ -22,7 +23,6 @@ const Gameboard = (function () {
 		// set a marker ( X or O) if the cell is empty
 		if (board[row][column] === '') {
 			board[row][column] = marker;
-			console.log('setting:', marker, 'at:', row, column);
 			return true;
 		} else return false;
 	};
@@ -45,143 +45,146 @@ const Player = function (name, marker) {
 	return { getName, getMarker };
 };
 
-// Game controller as an IIFE Module to manage the turns, win/tie logic and track current player
-const Game = (function (playerX, playerO) {
-	let gameOver = false;
-	let gameStatus;
+const initGame = function (nameX, nameO) {
+	const playerX = Player(nameX, 'X');
+	const playerO = Player(nameO, 'O');
 
-	const board = Gameboard.getBoard();
+	Game = (function (playerX, playerO) {
+		let gameOver = false;
+		let gameStatus;
 
-	const players = [playerX, playerO];
-	let currentPlayer = players[0];
-	let lastPlayer; // to keep track of who won
+		const board = Gameboard.getBoard();
 
-	const switchTurn = (player) => {
-		player =
-			player.getName() === players[0].getName() ? players[1] : players[0];
+		const players = [playerX, playerO];
+		let currentPlayer = players[0];
+		let lastPlayer; // to keep track of who won
 
-		currentPlayer = player;
-		return currentPlayer;
-	};
+		const switchTurn = (player) => {
+			player =
+				player.getName() === players[0].getName() ? players[1] : players[0];
 
-	// check for all winning combinations
-	const checkWinner = () => {
-		// check if the first element is no empty (if it is, there cannot be a win in that direction)
-		for (let i = 0; i < BOARD_SIZE; i++) {
-			// row
-			if (
-				board[i][0] !== '' &&
-				board[i][0] === board[i][1] &&
-				board[i][1] === board[i][2]
-			) {
-				return true;
-			}
-			// column
-			if (
-				board[0][i] !== '' &&
-				board[0][i] === board[1][i] &&
-				board[1][i] === board[2][i]
-			) {
-				return true;
-			}
-		}
-		// diagonal
-		// 0,0 - 1,1 - 2,2
-		// 0,2 - 1,1 - 2,0
-		if (board[1][1] !== '') {
-			if (
-				board[0][0] !== '' &&
-				board[0][0] === board[1][1] &&
-				board[1][1] === board[2][2]
-			) {
-				return true;
-			}
-			if (
-				board[0][2] !== '' &&
-				board[0][2] === board[1][1] &&
-				board[1][1] === board[2][0]
-			) {
-				return true;
-			}
-		}
+			currentPlayer = player;
+			return currentPlayer;
+		};
 
-		return false;
-	};
-
-	// checks if the board has empty spaces, if yes then it is not a tie yet, if no, it is a tie
-	const checkTie = () => {
-		for (let i = 0; i < BOARD_SIZE; i++) {
-			for (let j = 0; j < BOARD_SIZE; j++) {
-				if (board[i][j] === '') {
-					return false;
+		// check for all winning combinations
+		const checkWinner = () => {
+			// check if the first element is no empty (if it is, there cannot be a win in that direction)
+			for (let i = 0; i < BOARD_SIZE; i++) {
+				// row
+				if (
+					board[i][0] !== '' &&
+					board[i][0] === board[i][1] &&
+					board[i][1] === board[i][2]
+				) {
+					return true;
+				}
+				// column
+				if (
+					board[0][i] !== '' &&
+					board[0][i] === board[1][i] &&
+					board[1][i] === board[2][i]
+				) {
+					return true;
 				}
 			}
-		}
-		return true;
-	};
+			// diagonal
+			// 0,0 - 1,1 - 2,2
+			// 0,2 - 1,1 - 2,0
+			if (board[1][1] !== '') {
+				if (
+					board[0][0] !== '' &&
+					board[0][0] === board[1][1] &&
+					board[1][1] === board[2][2]
+				) {
+					return true;
+				}
+				if (
+					board[0][2] !== '' &&
+					board[0][2] === board[1][1] &&
+					board[1][1] === board[2][0]
+				) {
+					return true;
+				}
+			}
 
-	const restartGame = () => {
-		// game logic
-		gameOver = false;
-		Gameboard.resetBoard();
-	};
+			return false;
+		};
 
-	const checkGameStatus = () => {
-		if (checkWinner()) {
-			gameStatus = 'win';
-			gameOver = true;
-			return;
-		}
+		// checks if the board has empty spaces, if yes then it is not a tie yet, if no, it is a tie
+		const checkTie = () => {
+			for (let i = 0; i < BOARD_SIZE; i++) {
+				for (let j = 0; j < BOARD_SIZE; j++) {
+					if (board[i][j] === '') {
+						return false;
+					}
+				}
+			}
+			return true;
+		};
 
-		if (checkTie()) {
-			gameStatus = 'tie';
-			gameOver = true;
-			return;
-		}
-	};
+		const restartGame = () => {
+			// game logic
+			gameOver = false;
+			Gameboard.resetBoard();
+		};
 
-	const playRound = (row, column) => {
-		if (gameOver) return; // safe guard, in case the events are not unbinding on time.
+		const checkGameStatus = () => {
+			if (checkWinner()) {
+				gameStatus = 'win';
+				gameOver = true;
+				return;
+			}
 
-		// checks if the player chose an already-filled cell
-		if (Gameboard.setMarker(row, column, currentPlayer.getMarker())) {
-			// place the marker
-			const index = row * BOARD_SIZE + column;
-			Display.updateCell(index, currentPlayer.getMarker());
+			if (checkTie()) {
+				gameStatus = 'tie';
+				gameOver = true;
+				return;
+			}
+		};
 
+		const playRound = (row, column) => {
+			if (gameOver) return; // safe guard, in case the events are not unbinding on time.
+
+			// checks if the player chose an already-filled cell
+			if (Gameboard.setMarker(row, column, currentPlayer.getMarker())) {
+				// place the marker
+				const index = row * BOARD_SIZE + column;
+				Display.updateCell(index, currentPlayer.getMarker());
+			}
 			lastPlayer = currentPlayer;
+
+			checkGameStatus();
+			if (gameOver) {
+				Display.unbindCellEvents();
+				Display.showGameStatus();
+				// TODO: add a Display.gameOverDisplay function, to hold all these Display.sth funcs, display should only return gameOverDisplay
+				return;
+			}
 			switchTurn(currentPlayer);
-		}
+		};
 
-		checkGameStatus();
-		if (gameOver) {
-			Display.unbindCellEvents();
-			Display.showGameStatus();
-			// TODO: add a Display.gameOverDisplay function, to hold all these Display.sth funcs, display should only return gameOverDisplay
-			return;
-		}
-	};
-	const getLastPlayer = () => lastPlayer;
-	const getGameStatus = () => gameStatus;
+		const getLastPlayer = () => lastPlayer;
+		const getGameStatus = () => gameStatus;
 
-	return { playRound, getLastPlayer, getGameStatus };
-})(Player('playerX', 'X'), Player('playerO', 'O'));
+		return { playRound, getLastPlayer, getGameStatus };
+	})(Player('playerX', 'X'), Player('playerO', 'O'));
+};
 
 // DOM
 const Display = (function () {
 	// game mode
 	const cardGameMode = document.querySelector('#card__gameMode');
 	const modeTwoPlayers = document.querySelector('#mode__twoPlayers');
-	const modeVsComputer = document.querySelector('#mode__vsComputer');
 	// dialogs
 	const dialogTwoPlayers = document.querySelector('#dialog__twoPlayers');
-	const dialogVsComputer = document.querySelector('#dialog__vsComputer');
 	// forms
 	const form__twoPlayers = document.querySelector('#form__twoPlayers');
-	const form__vsComputer = document.querySelector('#form__vsComputer');
-
 	// board
 	const cells = document.querySelectorAll('.cell');
+
+	// players
+	let nameX, nameO;
 
 	// For both forms, click on start btn (type submit) will:
 	//  1) delete the game mode card
@@ -241,6 +244,9 @@ const Display = (function () {
 		cardGameMode.remove();
 		dialog.close();
 		renderBoard();
+		nameX = document.querySelector('#player__X').value;
+		nameO = document.querySelector('#player__O').value;
+		initGame(nameX, nameO);
 		bindCellEvents();
 	};
 
@@ -254,8 +260,6 @@ const Display = (function () {
 	const showGameStatus = () => {
 		const status = Game.getGameStatus();
 		const winnerMarker = Game.getLastPlayer().getMarker();
-		const playerX = document.querySelector('#player__X').value;
-		const playerO = document.querySelector('#player__O').value;
 
 		// DOM
 		const sectionGameStatus = document.querySelector('#section__gameStatus');
@@ -266,9 +270,9 @@ const Display = (function () {
 
 		if (status === 'win') {
 			if (winnerMarker === 'X') {
-				titleGameStatus.textContent = `🎉 ${playerX} wins! 🎉`;
+				titleGameStatus.textContent = `🎉 ${nameX} wins! 🎉`;
 			} else {
-				titleGameStatus.textContent = `🎉 ${playerO} wins! 🎉`;
+				titleGameStatus.textContent = `🎉 ${nameO} wins! 🎉`;
 			}
 		} else if (status === 'tie') {
 			titleGameStatus.textContent = `That's a tie!`;
@@ -286,16 +290,8 @@ const Display = (function () {
 			dialogTwoPlayers.showModal();
 		});
 
-		modeVsComputer.addEventListener('click', () => {
-			dialogVsComputer.showModal();
-		});
-
 		form__twoPlayers.addEventListener('submit', (e) => {
 			startGame(e, dialogTwoPlayers);
-		});
-
-		form__vsComputer.addEventListener('submit', (e) => {
-			startGame(e, dialogVsComputer);
 		});
 	};
 
